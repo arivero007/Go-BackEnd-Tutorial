a83ec45ceddbb1cd2d7f42d1d70c38d496cb2afd
arivero007 <32570344+arivero007@users.noreply.github.com>
2021-07-11 00:06:16 +0200

    Conexion a mongoDB

---
+++ bd/conexionBD.go
@@ -0,0 +1,36 @@
+package bd
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var MongoCN = ConectarBD()
+var clientOptions = options.Client().ApplyURI("mongodb+srv://arivero007:Oik35YaeQU0cQ5l1@cluster0.zunfv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+func ConectarBD() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+
+	log.Println("Conexion Exitosa con la BD")
+	return client
+}
+
+func ChequeoConnection() int {
+	err := MongoCN.Ping(context.TODO(), nil)
+	if err != nil {
+		return 0
+	}
+	return 1
+}
